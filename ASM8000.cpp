38327fc1f3f589e1b12c920d756e3c0e02438236
Michel Rochytor <michelrochytor.lb@gmail.com>
2025-07-12 01:31:30 -0300

    dpi e widgets do windows 11

--- ASM8000.cpp
+++ ASM8000.cpp
@@ -10,6 +10,7 @@ BOOL CALLBACK JanelaMain(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     hInst = hInstance;
 
+    SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
     // Cria a janela principal
     HWND hwnd1 = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)JanelaMain);
     if (hwnd1 == NULL) {
@@ -29,32 +30,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
     return (int)msg.wParam;
-}
-
-// Função de callback da janela principal
-BOOL CALLBACK JanelaMain(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch (msg) {
-    case WM_INITDIALOG:
-        // Inicialização da janela
-        return TRUE;
-
-    case WM_COMMAND:
-        // Tratamento de comandos (botões, menus, etc.)
-        switch (LOWORD(wParam)) {}
-        break;
-
-    case WM_CLOSE:
-        // Fechar a janela
-        DestroyWindow(hwnd);
-        break;
-
-    case WM_DESTROY:
-        // Encerrar a aplicação
-        PostQuitMessage(0);
-        break;
-
-    default:
-        return FALSE;
-    }
-    return TRUE;
 }
--- framework.h
+++ framework.h
@@ -14,3 +14,15 @@
 #include <memory.h>
 #include <tchar.h>
 #include <commctrl.h>
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <dwmapi.h>
+#pragma comment(lib, "dwmapi.lib")
+
+
+#include <ShellScalingApi.h>
+#pragma comment(lib, "Shcore.lib")
+#include "Janelas.h"
